f78ccee3ac08b3eb8bc6fa7a91ce4d208f37a9fb
Victor Vitalino <vitalino.victor@gmail.com>
2017-05-05 18:12:03 -0300

    fix

--- www/js/controllers/extranet.js
+++ www/js/controllers/extranet.js
@@ -3,9 +3,5 @@ var app = angular.module('codhab.controllers.extranet', []);
 app.controller('ExtraCtrl', function ($scope,$ionicPlatform, $ionicConfig, $state,$sce,$filter,$http) {
   $scope.url = $sce.trustAsResourceUrl("http://mobile.codhab.df.gov.br/protocol/conducts?deviceid="+window.localStorage['UserId']+'&pushtoken='+window.localStorage['PushToken']);
 
-  $scope.refresh = function() {
-  alert(window.localStorage['UserId'])
-  alert(window.localStorage['PushToken'])
-  }
 
 });
